f76f3ab9b4073731596bb09adfd8275f0de67ed4
jsw <csw9201@naver.com>
2022-08-03 15:03:14 +0900

    Drawing board: created "control line-width"

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const lineWidth = document.querySelector('#line-width');
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 canvas.width = 500;
 canvas.height = 500;
+ctx.lineWidth = lineWidth.value;
 
 const onPanting = (e) => {
   const x = e.offsetX;
@@ -16,6 +18,7 @@ const onCanvas = (e) => {
   const x = e.offsetX;
   const y = e.offsetY;
 
+  ctx.beginPath();
   ctx.moveTo(x, y);
 
   canvas.addEventListener('mousemove', onPanting);
@@ -25,5 +28,11 @@ const onCanvasOut = (e) => {
   canvas.removeEventListener('mousemove', onPanting);
 };
 
+const onChangeLineWidth = (e) => {
+  ctx.lineWidth = e.target.value;
+};
+
 canvas.addEventListener('mousedown', onCanvas);
 canvas.addEventListener('mouseup', onCanvasOut);
+
+lineWidth.addEventListener('change', onChangeLineWidth);
