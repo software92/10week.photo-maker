56f265112273abb5342394864233fe5da4ebf993
jsw <csw9201@naver.com>
2022-08-03 19:38:36 +0900

    Drawing board: created "palette", updated "draw a line"(mouseleave)

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const lineWidth = document.querySelector('#line-width');
+const colors = document.querySelectorAll('.colors div');
+
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
@@ -32,7 +34,17 @@ const onChangeLineWidth = (e) => {
   ctx.lineWidth = e.target.value;
 };
 
+const onChangeColor = (e) => {
+  ctx.strokeStyle = e.target.dataset.color;
+};
+
+colors.forEach((color) => {
+  color.style.backgroundColor = color.dataset.color;
+  color.addEventListener('click', onChangeColor);
+});
+
 canvas.addEventListener('mousedown', onCanvas);
 canvas.addEventListener('mouseup', onCanvasOut);
+canvas.addEventListener('mouseleave', onCanvasOut);
 
 lineWidth.addEventListener('change', onChangeLineWidth);
