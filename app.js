3fcdb6cdde951062efa74fc23df784e68b2eba32
jsw <csw9201@naver.com>
2022-08-03 14:39:11 +0900

    Drawing board: created "draw a line"

--- app.js
+++ app.js
@@ -3,3 +3,27 @@ const ctx = canvas.getContext('2d');
 
 canvas.width = 500;
 canvas.height = 500;
+
+const onPanting = (e) => {
+  const x = e.offsetX;
+  const y = e.offsetY;
+
+  ctx.lineTo(x, y);
+  ctx.stroke();
+};
+
+const onCanvas = (e) => {
+  const x = e.offsetX;
+  const y = e.offsetY;
+
+  ctx.moveTo(x, y);
+
+  canvas.addEventListener('mousemove', onPanting);
+};
+
+const onCanvasOut = (e) => {
+  canvas.removeEventListener('mousemove', onPanting);
+};
+
+canvas.addEventListener('mousedown', onCanvas);
+canvas.addEventListener('mouseup', onCanvasOut);
